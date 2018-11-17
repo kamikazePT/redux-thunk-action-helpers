420fad860daf344279538fffff54503d515fe9df
kamikazePT <afonso.1995@hotmail.com>
2018-11-17 23:49:53 +0000

    fixed namespace

--- src/actions/types/create_action_types.js
+++ src/actions/types/create_action_types.js
@@ -2,6 +2,10 @@ import { createTypes, asyncAction } from 'redux-action-types';
 import { flatMap } from 'lodash';
 import actionConstants from '../../domain/action_types_constants';
 
-export default (namespace, ...args) => createTypes('@@' + namespace + '/',
-  flatMap(args, (item) => [item].concat(asyncAction(item, [ actionConstants.START, actionConstants.SUCCESS, actionConstants.ERROR])))
-);
+export default (namespace, ...args) => {
+  let finalNamespace = namespace;
+  if(finalNamespace) finalNamespace = '@@' + finalNamespace + '/';
+  return createTypes(finalNamespace,
+    flatMap(args, (item) => [item].concat(asyncAction(item, [ actionConstants.START, actionConstants.SUCCESS, actionConstants.ERROR])))
+  );
+};
